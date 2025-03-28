7f382c81284f093070d05daf286d90319b36660e
Rafael Rossi <rossirafael@yahoo.com>
2025-03-28 22:17:13 +0100

    Proper scrambling with animation

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,66 @@
-use std::env;
+use std::{env, time, thread};
+use nannou::prelude::*;
 
-use nannou::{prelude::*, rand::{seq::SliceRandom, thread_rng}};
 
 static PAD_HEIGHT_FACTOR: f32 = 0.1;
-static GRID_HEIGHT: usize = 4;
-static GRID_WIDTH: usize = 4;
 
-// Build a solved board with numbers up to GRID_HEIGHT * GRID_WIDTH - 1
-fn solved_board() -> Vec<Vec<usize>> {
-    let mut board = vec![vec![0; GRID_WIDTH]; GRID_HEIGHT];
-    let mut count = 1;
-    for row in 0..GRID_HEIGHT {
-        for col in 0..GRID_WIDTH {
-            board[row][col] = count;
-            count += 1;
+
+// Build a solved board with numbers up to height * width - 1
+fn solved_board(height: usize, width: usize) -> Vec<Vec<usize>> {
+    let mut board = vec![vec![0; width]; height];
+    for row in 0..height {
+        for col in 0..width {
+            board[row][col] = (height - row - 1) * width + col + 1;
         }
     }
-    board[GRID_HEIGHT - 1][GRID_WIDTH - 1] = 0;
+    board[0][width - 1] = 0;
     board
 }
 
-// Build a board with numbers up to GRID_HEIGHT * GRID_WIDTH - 1
+// Build a board with numbers up to height * width - 1
 // in a scrambled order.
-fn scrambled_board() -> Vec<Vec<usize>> {
-    
-    // vector of numbers from 0 to GRID_HEIGHT * GRID_WIDTH - 1 in a random order
-    let mut numbers: Vec<usize> = (0..GRID_HEIGHT * GRID_WIDTH).collect();
-    numbers.shuffle(&mut thread_rng());
-
-    let mut board = vec![vec![0; GRID_WIDTH]; GRID_HEIGHT];
-    for i in 0..GRID_HEIGHT {
-        for j in 0..GRID_WIDTH {
-            board[i][j] = numbers.pop().unwrap();
-        }
-    }
-    board
-}
+// TODO: Not sure the resultant board is solvable!
+//fn scrambled_board(height: usize, width: usize) -> Vec<Vec<usize>> {
+//    // vector of numbers from 0 to height * width - 1 in a random order
+//    let mut numbers: Vec<usize> = (0..height * width).collect();
+//    numbers.shuffle(&mut thread_rng());
+//
+//    let mut board = vec![vec![0; width]; height];
+//    for i in 0..height {
+//        for j in 0..width {
+//            board[i][j] = numbers.pop().unwrap();
+//        }
+//    }
+//    board
+//}
 
 struct Model {
+    height: usize,
+    width: usize,
+    flag_scramble: bool,
+    scramble_count: usize,
     board: Vec<Vec<usize>>,
 }
 impl Model {
-    fn new() -> Self {
+    fn new(h: usize, w: usize) -> Self {
         Self {
-            board: scrambled_board(),
+            height: h,
+            width: w,
+	    flag_scramble: false,
+	    scramble_count: 0,
+            board: solved_board(h, w),
         }
     }
     /// Reset board
     fn reset(&mut self) {
-        self.board = scrambled_board();
-    }
-    /// Solve board
-    fn solve(&mut self) {
-        self.board = solved_board();
+        self.board = solved_board(self.height, self.width);
     }
 
     /// Returns the indices of the empty space.
     fn index_empty(&self) -> (usize, usize) {
-
         let iy = self.board.iter().position(|r| r.contains(&0)).unwrap();
         let ix = self.board[iy].iter().position(|&x| x == 0).unwrap();
-       
+
         (ix, iy)
     }
 
@@ -76,29 +76,56 @@ impl Model {
     /// Move the piece at `(ix, iy)` to the empty space.
     /// Check if the move is valid.
     fn try_move(&mut self, ix: usize, iy: usize) {
-        println!("Trying to move piece at index {ix}, {iy}");
+        //println!("Trying to move piece at index {ix}, {iy}");
         match self.is_move_valid(ix, iy) {
             true => {
-                println!("Move is valid");
+                //println!("Move is valid");
                 let (empty_x, empty_y) = self.index_empty();
                 self.board[empty_y][empty_x] = self.board[iy][ix];
                 self.board[iy][ix] = 0;
             }
             false => {
-                println!("Move is invalid");
+                //println!("Move is invalid");
                 ()
             }
         }
     }
+
+    /// Scramble the puzzle by randomly clicking everywhere
+    fn scramble(&mut self) {
+	loop {
+	    let ix = random_range(0, self.width);
+	    let iy = random_range(0, self.height);
+	    if self.is_move_valid(ix, iy) {
+		self.try_move(ix, iy);
+		return
+	    }
+	}
+    }
 }
 
 fn main() {
-    nannou::app(model).loop_mode(LoopMode::Wait).run();
+    nannou::app(model)
+	.update(update)
+	.loop_mode(LoopMode::Wait)
+	.run();
 }
 
 fn model(app: &App) -> Model {
+    let args: Vec<_> = env::args().collect();
 
-    println!("Args: {:?}", env::args().collect::<Vec<_>>());
+    let (height, width) = match args.len() {
+        2 => {
+            let size = args[1].parse().unwrap();
+            (size, size)
+        }
+        3 => {
+            let height = args[1].parse().unwrap();
+            let width = args[2].parse().unwrap();
+            (height, width)
+        }
+        _ => (4, 4),
+    };
 
     let _window = app
         .new_window()
@@ -108,7 +135,21 @@ fn model(app: &App) -> Model {
         .event(event)
         .build()
         .unwrap();
-    Model::new()
+
+    Model::new(height, width)
+}
+
+fn update(app: &App, model: &mut Model, _update: Update) {
+    if model.flag_scramble {
+	model.scramble();
+	thread::sleep(time::Duration::from_millis(15));
+	model.scramble_count += 1;
+	if model.scramble_count > 100 {
+	    model.scramble_count = 0;
+	    model.flag_scramble = false;
+	    app.set_loop_mode(LoopMode::Wait);
+	}
+    }
 }
 
 fn event(app: &App, model: &mut Model, event: WindowEvent) {
@@ -116,8 +157,6 @@ fn event(app: &App, model: &mut Model, event: WindowEvent) {
         MousePressed(_button) => {
             // Check if the user clicked on an arrow
             // and move it if it can be moved.
-            println!("Playing mode");
-            println!("Clicked at {}, {}", app.mouse.x, app.mouse.y);
             let win = app.window_rect();
             let pad = win.h() * PAD_HEIGHT_FACTOR;
             let w = win.w();
@@ -125,18 +164,24 @@ fn event(app: &App, model: &mut Model, event: WindowEvent) {
             if (win.top() - app.mouse.y)
                 .min(app.mouse.y - win.bottom())
                 .min(win.right() - app.mouse.x)
-                .min(app.mouse.x - win.left()) < pad 
+                .min(app.mouse.x - win.left())
+                < pad
             {
-                println!("Clicked outside the board");
+                //println!("Clicked outside the board");
                 return;
             }
-            let ix_clicked = (GRID_WIDTH as f32 * (app.mouse.x + w / 2.0) / w) as usize;
-            let iy_clicked = (GRID_HEIGHT as f32 * (app.mouse.y + h / 2.0) / h) as usize;
-            println!("Indices clicked: {}, {}", ix_clicked, iy_clicked);
+            let ix_clicked =
+                (model.width as f32 * (app.mouse.x + w / 2.0 - pad) / (w - 2.0 * pad)) as usize;
+            let iy_clicked =
+                (model.height as f32 * (app.mouse.y + h / 2.0 - pad) / (h - 2.0 * pad)) as usize;
+            //println!("Indices clicked: {}, {}", ix_clicked, iy_clicked);
             model.try_move(ix_clicked, iy_clicked);
-        },
+        }
         KeyPressed(Key::R) => model.reset(),
-        KeyPressed(Key::S) => model.solve(),
+	KeyPressed(Key::S) => {
+	    app.set_loop_mode(LoopMode::RefreshSync);
+	    model.flag_scramble = true;
+	},
         _ => (),
     }
 }
@@ -149,14 +194,15 @@ fn view(app: &App, model: &Model, frame: Frame) {
     // draw the board
     let win = app.window_rect();
     let pad = win.h() * PAD_HEIGHT_FACTOR;
-    let cell_width = (win.w() - 2.0 * pad) / GRID_WIDTH as f32;
-    let cell_height = (win.h() - 2.0 * pad) / GRID_HEIGHT as f32;
+    let cell_width = (win.w() - 2.0 * pad) / model.width as f32;
+    let cell_height = (win.h() - 2.0 * pad) / model.height as f32;
     let font_size = (cell_height.min(cell_width) / 2.0) as u32;
 
     // draw all the cells
-    for row in 0..GRID_HEIGHT {
+    for row in 0..model.height {
         let y = win.bottom() + pad + row as f32 * cell_height + cell_height / 2.0;
-        for col in 0..GRID_WIDTH {
+
+        for col in 0..model.width {
             let x = win.left() + pad + col as f32 * cell_width + cell_width / 2.0;
 
             let piece = model.board[row][col];
@@ -175,15 +221,13 @@ fn view(app: &App, model: &Model, frame: Frame) {
                 .stroke_weight(2.0);
 
             // draw the number of the piece
-            
 
             let text = match piece {
                 0 => String::from(""),
                 _ => piece.to_string(),
             };
 
-            let text_area = geom::Rect::from_w_h(cell_width, cell_height)
-                .relative_to([-x, -y]);
+            let text_area = geom::Rect::from_w_h(cell_width, cell_height).relative_to([-x, -y]);
 
             draw.text(&text)
                 .font_size(font_size)
